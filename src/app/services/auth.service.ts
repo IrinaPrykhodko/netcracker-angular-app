078582a9f2a6a63387a33b40261e4b6b9b74aa56
IrinaPrykhodko <ir.prikhodko@gmail.com>
2019-12-06 12:43:38 +0200

    add AuthService

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {User} from "../models/user";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(cread: User){
+    return this.http.post("login", cread);
+  }
+}
