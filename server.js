478d6d6172be1f7a71aa9d89fda04ca5216907fd
IrinaPrykhodko <ir.prikhodko@gmail.com>
2019-12-10 16:02:15 +0200

    add settings for heroku 2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/app'));
+app.use(express.static(__dirname + '/dist/netcracker-angular-app'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/netcracker-angular-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
