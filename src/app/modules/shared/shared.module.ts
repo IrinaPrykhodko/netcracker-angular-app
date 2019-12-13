20dee8290a954af90046c6fce464af6250ad73a1
IrinaPrykhodko <ir.prikhodko@gmail.com>
2019-12-13 12:35:44 +0200

    add shared module

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {NgxIntlTelInputModule} from 'ngx-intl-tel-input';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule,
+    NgxIntlTelInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    FormsModule,
+    CommonModule,
+    NgxIntlTelInputModule
+  ]
+})
+export class SharedModule { }
