bf4f512adc37d0c897b9c13781420f8da54b8f81
Yamiguri <ududenko.s98@gmail.com>
2019-12-16 00:53:25 +0200

    sidebar works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './modules/login/components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginRoutingModule } from './modules/login/login-routing.module';
-import {RegisterComponent} from './modules/register/сomponents/register/register.component';
 import {NgxIntlTelInputModule} from 'ngx-intl-tel-input';
 import {LoginModule} from './modules/login/login.module';
 import {RegisterModule} from './modules/register/register.module';
