14930feb816d389da250021d21d7a9761e635895
AleksandrBut <alex638000@gmail.com>
2019-12-27 00:22:57 +0200

    implement ErrorInterceptorService.intercept method

--- src/app/services/error-interceptor.service.ts
+++ src/app/services/error-interceptor.service.ts
@@ -11,19 +11,27 @@ export class ErrorInterceptorService implements HttpInterceptor {
   constructor() {
   }
 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage: string;
+
+    if (error.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      errorMessage = `Error status: ${error.status}`;
+
+      if (error.status === 500) {
+        alert('Sorry, try again later');
+        errorMessage += (`\nMessage: ${error.message}`);
+      }
+    }
+
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(request)
-      .pipe(catchError((error: HttpErrorResponse) => {
-        let errorMessage: string;
-
-        if (error.error instanceof ErrorEvent) {
-          errorMessage = `Error: ${error.error.message}`;
-        } else {
-          errorMessage = `Error status: ${error.status}\nMessage: ${error.message}`;
-        }
-
-        console.log(errorMessage);
-        return throwError(errorMessage);
-      }));
+    return next.handle(request).pipe(
+      catchError(this.handleError)
+    );
   }
 }
