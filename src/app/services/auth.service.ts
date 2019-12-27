deab43b20f10f3dd2caf95c282510585aae96905
AleksandrBut <alex638000@gmail.com>
2019-12-27 12:52:56 +0200

    add token management to AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,39 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {User} from '../models/user';
 import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  private currentUserTokenSubject: BehaviorSubject<string>;
+  private currentUserToken: Observable<string>;
+  private tokenKey = 'token';
 
-  login(cread: User) {
-    return this.http.post('https://med-kit.herokuapp.com/login', cread);
+  constructor(private http: HttpClient) {
+    this.currentUserTokenSubject = new BehaviorSubject<string>(JSON.parse(localStorage.getItem(this.tokenKey)));
+    this.currentUserToken = this.currentUserTokenSubject.asObservable();
+  }
+
+  public get currentUserTokenValue(): string {
+    return this.currentUserTokenSubject.value;
+  }
+
+  login(cred: User) {
+    return this.http.post('https://med-kit.herokuapp.com/login', cred)
+      .pipe(map(token => {
+        const tokenAsString = JSON.stringify(token);
+        localStorage.setItem(this.tokenKey, tokenAsString);
+        this.currentUserTokenSubject.next(tokenAsString);
+
+        return tokenAsString;
+      }));
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
   }
 }
