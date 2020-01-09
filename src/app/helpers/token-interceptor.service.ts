e2f144e9110630f58a5c7dd9882910637606ed2f
AleksandrBut <alex638000@gmail.com>
2020-01-09 20:48:36 +0200

    do not add token to request header from some routes

--- src/app/helpers/token-interceptor.service.ts
+++ src/app/helpers/token-interceptor.service.ts
@@ -2,26 +2,32 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private router: Router) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.getCurrentUserToken();
+    const currentRoute = this.router.url;
 
-    if (token != null) {
-      req = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`
-        }
-      });
+    if (currentRoute !== '/login' && currentRoute !== '/register') {
+      const token = this.authService.getCurrentUserToken();
+
+      if (token != null) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+      }
+
     }
-
     return next.handle(req);
   }
 }
