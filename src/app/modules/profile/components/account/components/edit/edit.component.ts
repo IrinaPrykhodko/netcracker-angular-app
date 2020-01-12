5e95f64d89fd9771d42bbbce86a8e900ffd1b5cf
AleksandrBut <alex638000@gmail.com>
2020-01-12 22:30:20 +0200

    fix ts-lint errors

--- src/app/modules/profile/components/account/components/edit/edit.component.ts
+++ src/app/modules/profile/components/account/components/edit/edit.component.ts
@@ -5,58 +5,58 @@ import {Patient} from '../../../../../../models/patient';
 import {Router} from '@angular/router';
 
 @Component({
-    selector: 'app-edit',
-    templateUrl: './edit.component.html',
-    styleUrls: ['./edit.component.scss']
+  selector: 'app-edit',
+  templateUrl: './edit.component.html',
+  styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
 
-    public patient: Patient = new Patient();
-    public editForm: FormGroup;
+  public patient: Patient = new Patient();
+  public editForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder,
-                private patientService: PatientService,
-                private router: Router) {
-    }
+  constructor(private formBuilder: FormBuilder,
+              private patientService: PatientService,
+              private router: Router) {
+  }
 
-    ngOnInit() {
-        this.patientService.getPatient().subscribe((data: Patient) => this.patient = data);
-        this.editForm = this.formBuilder.group({
-            firstName: [this.patient.firstName, [Validators.required]],
-            lastName: [this.patient.lastName, [Validators.required]],
-            dateOfBirth: [this.patient.dateOfBirth],
-            height: [this.patient.height],
-            weight: [this.patient.weight],
-            location: [this.patient.location],
-            phoneNumber: [this.patient.phoneNumber, [Validators.required]],
-            email: [this.patient.email, [Validators.email, Validators.required]],
-        });
-    }
+  ngOnInit() {
+    this.patientService.getPatient().subscribe((data: Patient) => this.patient = data);
+    this.editForm = this.formBuilder.group({
+      firstName: [this.patient.firstName, [Validators.required]],
+      lastName: [this.patient.lastName, [Validators.required]],
+      dateOfBirth: [this.patient.dateOfBirth],
+      height: [this.patient.height],
+      weight: [this.patient.weight],
+      location: [this.patient.location],
+      phoneNumber: [this.patient.phoneNumber, [Validators.required]],
+      email: [this.patient.email, [Validators.email, Validators.required]],
+    });
+  }
 
-    get email() {
-        return this.editForm.get('email');
-    }
+  get email() {
+    return this.editForm.get('email');
+  }
 
-    get firstName() {
-        return this.editForm.get('firstName');
-    }
+  get firstName() {
+    return this.editForm.get('firstName');
+  }
 
-    public get lastName() {
-        return this.editForm.get('lastName');
-    }
+  public get lastName() {
+    return this.editForm.get('lastName');
+  }
 
-    get phoneNumber() {
-        return this.editForm.get('phoneNumber');
-    }
+  get phoneNumber() {
+    return this.editForm.get('phoneNumber');
+  }
 
-    submit() {
-        console.log(this.editForm.value);
-        this.patientService.editPatient(this.editForm.value)
-            .subscribe((userData) => {
-                console.log(userData);
-                this.router.navigate(['/profile/account']);
-            }, (error => {
-                console.log(error);
-            }));
-    }
+  submit() {
+    console.log(this.editForm.value);
+    this.patientService.editPatient(this.editForm.value)
+      .subscribe((userData) => {
+        console.log(userData);
+        this.router.navigate(['/profile/account']);
+      }, (error => {
+        console.log(error);
+      }));
+  }
 }
