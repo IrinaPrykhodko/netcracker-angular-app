2d4708b074175e24407bf30dda6f6bf20ccb9716
AleksandrBut <alex638000@gmail.com>
2020-01-21 23:10:34 +0200

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://med-kit.herokuapp.com/',
   purchaseApiUrl: 'https://e-pharmacy.herokuapp.com'
 };
