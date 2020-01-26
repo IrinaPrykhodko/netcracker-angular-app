51525ae08838772fe463a9f420512c8c5e96cbde
AleksandrBut <alex638000@gmail.com>
2020-01-26 18:26:54 +0200

    use Observable to get current user data, add spinner

--- src/app/modules/profile/components/profile/profile.component.ts
+++ src/app/modules/profile/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PatientService} from '../../../../services/patient.service';
+import {finalize} from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  private isLoading;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private patientService: PatientService) {
   }
 
+  ngOnInit() {
+    this.isLoading = true;
+
+    this.patientService.getPatient()
+      .pipe(
+        finalize(() => this.isLoading = false)
+      )
+      .subscribe();
+  }
 }
