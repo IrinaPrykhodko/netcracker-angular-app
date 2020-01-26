37d2773d18e5c4a485b0159910b0977c1cfb5d42
AleksandrBut <alex638000@gmail.com>
2020-01-26 18:16:54 +0200

    add user BehaviourSubject

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -2,17 +2,26 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Patient} from '../models/patient';
 import {environment} from '../../environments/environment';
+import {map} from 'rxjs/operators';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
 
+  private user = new BehaviorSubject(null);
+  public user$ = this.user.asObservable();
+
   constructor(private http: HttpClient) {
   }
 
   getPatient() {
-    return this.http.get(`${environment.apiUrl}/account`);
+    return this.http.get(`${environment.apiUrl}/account`)
+      .pipe(map(user => {
+        this.user.next(user);
+        return user;
+      }));
   }
 
   editPatient(cred: Patient) {
