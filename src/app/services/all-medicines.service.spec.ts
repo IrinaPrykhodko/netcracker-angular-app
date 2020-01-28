e237d75999d50f36584a7c46d2e757134bfbc448
AleksandrBut <alex638000@gmail.com>
2020-01-28 22:53:58 +0200

    rename MedicineService

--- src/app/services/all-medicines.service.spec.ts
+++ src/app/services/all-medicines.service.spec.ts
@@ -1,12 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AllMedicinesService } from './all-medicines.service';
+import {MedicineService} from './amedicine.service';
 
 describe('AllMedicinesService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: AllMedicinesService = TestBed.get(AllMedicinesService);
+    const service: MedicineService = TestBed.get(MedicineService);
     expect(service).toBeTruthy();
   });
 });
--- src/app/services/amedicine.service.ts
+++ src/app/services/amedicine.service.ts
@@ -7,7 +7,7 @@ import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class AllMedicinesService {
+export class MedicineService {
 
   constructor(private http: HttpClient) {
   }
