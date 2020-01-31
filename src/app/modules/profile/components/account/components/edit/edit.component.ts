e03cc5fcc03ef9862e05c5c9f687911ebc9d016a
AleksandrBut <alex638000@gmail.com>
2020-02-01 01:17:32 +0200

    OD19DEVG5-61 add firstName and lastName validation: fields can not contain numbers

--- src/app/modules/profile/components/account/components/edit/edit.component.ts
+++ src/app/modules/profile/components/account/components/edit/edit.component.ts
@@ -26,8 +26,8 @@ export class EditComponent implements OnInit {
 
   ngOnInit() {
     this.editForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
+      lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z]*')]],
       dateOfBirth: [''],
       height: ['', Validators.min(0)],
       weight: ['', Validators.min(0)],
