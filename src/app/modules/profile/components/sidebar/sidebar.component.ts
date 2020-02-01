4c014d7803688a2d5e0b6d6b95eee7b1d209e14d
IrinaPrykhodko <ir.prikhodko@gmail.com>
2020-02-02 00:32:21 +0200

    OD19DEVG5-82
    add notification counter

--- src/app/modules/profile/components/sidebar/sidebar.component.ts
+++ src/app/modules/profile/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NotificationService} from '../../../../services/notification.service';
+import {Notification} from '../../../../models/notification';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  public notificationList: Notification[];
+  public reminderList: Notification[];
+  public notificationsAndRemindersCount = 0;
+
+  constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.notificationService.reminderAndNotificationCount$
+      .subscribe(count => this.notificationsAndRemindersCount = count);
   }
 
 }
--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Patient} from '../../../../models/patient';
 import {PatientService} from '../../../../services/patient.service';
 import {AuthService} from '../../../../services/auth.service';
+import {NotificationService} from '../../../../services/notification.service';
 
 @Component({
   selector: 'app-header',
@@ -11,17 +12,24 @@ import {AuthService} from '../../../../services/auth.service';
 export class HeaderComponent implements OnInit {
 
   public user: Patient;
+  public notificationList: Notification[];
+  public reminderList: Notification[];
+  public notificationsAndRemindersCount = 0;
 
   constructor(private patientService: PatientService,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private notificationService: NotificationService) {
   }
 
   ngOnInit() {
     this.patientService.user$.subscribe(user => this.user = user);
+    this.notificationService.reminderAndNotificationCount$.subscribe(count => this.notificationsAndRemindersCount = count);
   }
 
   logout() {
     this.authService.logout();
     this.user = undefined;
+    this.notificationList = undefined;
+    this.reminderList = undefined;
   }
 }
