170f8f6a15dd67cac22e1cd9df81e4c421047908
Yamiguri <ududenko.s98@gmail.com>
2020-02-06 16:17:59 +0200

    OD19DEVG5-114
    unsubsribe account

--- src/app/modules/profile/components/account/account.component.ts
+++ src/app/modules/profile/components/account/account.component.ts
@@ -1,20 +1,23 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Patient} from '../../../../models/patient';
 import {PatientService} from '../../../../services/patient.service';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {EditComponent} from './components/edit/edit.component';
 import {ChangePasswordComponent} from './components/change-password/change-password.component';
+import {Subject} from "rxjs";
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
-export class AccountComponent implements OnInit {
+export class AccountComponent implements OnInit, OnDestroy {
 
   patient: Patient;
   public dialogRefEdit: MatDialogRef<EditComponent>;
   public dialogRefChange: MatDialogRef<ChangePasswordComponent>;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private patientService: PatientService,
               private dialog: MatDialog) {
@@ -22,6 +25,9 @@ export class AccountComponent implements OnInit {
 
   ngOnInit() {
     this.patientService.user$
+      .pipe(
+        takeUntil(this.destroy$)
+      )
       .subscribe((data: Patient) => {
         this.patient = data;
       });
@@ -42,4 +48,10 @@ export class AccountComponent implements OnInit {
   changePassword() {
     this.dialogRefChange = this.dialog.open(ChangePasswordComponent);
   }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
 }
