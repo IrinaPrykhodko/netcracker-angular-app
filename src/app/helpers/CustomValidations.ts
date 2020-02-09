a15cc6ec124927545e4b149677ecd3858ebc2b65
AleksandrBut <alex638000@gmail.com>
2020-02-09 17:08:31 +0200

    OD19DEVG5-61 add CustomValidations class

---
+++ src/app/helpers/CustomValidations.ts
@@ -0,0 +1,27 @@
+import {AbstractControl, ValidatorFn} from '@angular/forms';
+
+export class CustomValidations {
+
+  static matchPatternOrEmpty(pattern: RegExp): ValidatorFn {
+    return ((control: AbstractControl): { [key: string]: any } | null => {
+        const value = control.value;
+
+        if (value) {
+          return pattern.test(value) ? null : {matchPatternOrEmpty: true};
+        }
+
+        return null;
+      }
+    );
+  }
+
+  static digitsOnlyOrEmpty(control: AbstractControl): { [key: string]: any } | null {
+    const valueAsNumber = +control.value;
+
+    return String(control.value).trim() === '' || isNaN(valueAsNumber) ? {digitsOnlyOrEmpty: true} : null;
+  }
+
+  static digitsOnly(control: AbstractControl): { [key: string]: any } | null {
+    return control.value === null ? {digitsOnly: true} : null;
+  }
+}
